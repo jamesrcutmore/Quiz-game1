0905287ecca64009c2c1bb95362ebed28c96aaed
jamesrcutmore <robcutmore36@gmail.com>
2022-06-17 18:17:43 +0000

    js code change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) }
+document.addEventListener("DOMContentLoaded", function(event) '{'
 const quizDB = [
     {
         question: "Q1: What is the full form of HTML?",
